f5295d8872a58de2b476a3f20ff894d55b3fe0ae
Raitah Jinnat <raitahjinnat@gmail.com>
2025-03-21 14:55:15 -0400

    filter form created

--- frontend/src/pages/Dashboard/Resources.js
+++ frontend/src/pages/Dashboard/Resources.js
@@ -1,3 +1,32 @@
 import React {useState, useEffect} from 'react';
 import "./Resources.css"
 
+function Resources() {
+
+    return (
+        <div className="resources-container">
+            <div className="filter">
+                <div>
+                    <label>Search by keyword: </label>
+                    <input
+                        type="text"
+                        placeholder="Search for a keyword"
+                    />
+                </div>
+
+                <div>
+                    <label>Start Date: </label>
+                    <input
+                        type="date"
+                    />
+          <label>End Date: </label>
+          <input
+            type="date"
+          />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Resources;
