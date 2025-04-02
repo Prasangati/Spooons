d183224ffc43dace4c1ed9aab78473c3deb2f940
prasangatiwari <prasangahere@gmail.com>
2025-04-01 20:55:36 -0400

    Update allowed hosts for render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]"]
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 # Application definition
 
 INSTALLED_APPS = [
