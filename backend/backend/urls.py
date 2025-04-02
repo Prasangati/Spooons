afedfe052262008c68f65c41a9ea85d09c6ff691
prasangatiwari <prasangahere@gmail.com>
2025-04-01 21:07:11 -0400

    import json error resolved

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 from django.http import JsonResponse
-# Just a placeholder
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
