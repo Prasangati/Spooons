d39223395f476b1cb9afcbc69ecb6a6c743a0304
prasangatiwari <prasangahere@gmail.com>
2025-04-05 22:12:48 -0400

    Cleaned up the files

--- frontend/src/components/journals/RecentEntries.jsx
+++ frontend/src/components/journals/RecentEntries.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Loading from "../../pages/Loading/Loading";
-import BASE_URL from '../../utils/config';
+import BASE_URL from "../../utils/config";
 
 
 const RecentEntries = () => {
