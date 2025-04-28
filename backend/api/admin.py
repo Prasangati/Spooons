eacc7f4ed6a723eb8835aac0b658e2044624e391
prasangatiwari <prasangahere@gmail.com>
2025-04-28 01:05:41 -0400

    Basic css

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,18 +1,13 @@
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib import admin
-from api.models import CustomUser
-from journal_entries.models import JournalEntry
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
+from .models import CustomUser
 
 class CustomUserAdmin(BaseUserAdmin):
     model = CustomUser
-    form = UserChangeForm
-    add_form = UserCreationForm
-    ordering = ('email',)
-    list_display = ('email', 'first_name', 'last_name', 'is_staff')
-    search_fields = ('email', 'first_name', 'last_name')
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'is_superuser', 'groups')
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
@@ -22,14 +17,13 @@ class CustomUserAdmin(BaseUserAdmin):
         }),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
-
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'first_name', 'password1', 'password2'),
-        }),
+            'fields': ('email', 'first_name', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
     )
-
+    search_fields = ('email',)
+    ordering = ('email',)
 
 admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(JournalEntry)
