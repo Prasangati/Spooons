63067e722d415c0baef682bbb189792fe9513a8f
Raitah Jinnat <raitahjinnat@gmail.com>
2025-05-04 14:10:19 -0400

    working on implementing calendar implent with import statements

---
+++ frontend/src/pages/Dashboard/Progress.js
@@ -0,0 +1,4 @@
+import React, {useContext, useState } from "react";
+import { JournalContext } from "../context/JournalContext";
+import JournalCalendar from "../components/JournalCalendar";
+import "./Progress.css";
