864daebd7e2dc599ff1f189dffcd972e5f088f86
Raitah Jinnat <raitah.jinnat83@myhunter.cuny.edu>
2025-05-07 15:58:49 -0400

    progress container

--- frontend/src/pages/Dashboard/Progress.js
+++ frontend/src/pages/Dashboard/Progress.js
@@ -4,7 +4,11 @@ import axios from "axios";
 import JournalEntries from "./Journal";
 
 function Progress() {
-
-}
+    return (
+        <div className="progress-container">
+            <p className="subtext">See your journaling patterns over time...</p>
+        </div>
+    );
+};
 
 export default Progress;
